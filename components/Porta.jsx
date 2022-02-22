7356b843ac8944453b6b75b6c54700c08b13f107
Clesio <kornalewski@outlook.com>
2022-02-21 21:20:23 -0300

    main

---
+++ components/Porta.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Porta.module.css'
+
+export default function Porta(props) {
+    return (
+        <div className={styles.porta}>
+            <div className={styles.numero}>8</div>
+        </div>
+    )
+}
