18ebde2550d2d382b7d51bc2f5b136fa762c9f56
Clesioks <kornalewski@outlook.com>
2022-03-03 17:49:00 -0300

    link para página inicial

---
+++ pages/jogo.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useState } from "react";
+import Porta from "../components/Porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
+import styles from "../styles/Jogo.module.css";
+
+export default function jogo() {
+  const [portas, setPortas] = useState(criarPortas(8, 3));
+
+  function renderizarPortas() {
+    return portas.map((porta) => {
+      return (
+        <Porta
+          key={porta.numero}
+          value={porta}
+          onChange={(novaPorta) =>
+            setPortas(atualizarPortas(portas, novaPorta))
+          }
+        />
+      );
+    });
+  }
+
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>{renderizarPortas()}</div>
+      <div className={styles.botoes}>
+        <Link href="/">
+          <button>Reiniciar Jogo</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
